d7be2bc5f35d2af821e9fb0658c80751f0d11478
Arya More <arya.more.edu4@gmail.com>
2026-02-26 18:48:16 -0800

    Added "shell1.c" and repurposed it to fit assignment

---
+++ shell1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/wait.h>
+#define MAXLINE 4096
+
+void err_sys(const char* c) {
+	perror(c);
+	exit(1);
+}
+void err_ret(const char* c, ...) {
+	va_list ap;
+	va_start(ap, c);
+	fprintf(stderr, c, ap);
+	va_end(ap);
+}
+int main(void)
+{
+	char	buf[MAXLINE];
+	pid_t	pid;
+	int		status;
+
+	printf("%% ");	/* print prompt (printf requires %% to print %) */
+	while (fgets(buf, MAXLINE, stdin) != NULL) {
+		if (buf[strlen(buf) - 1] == '\n')
+			buf[strlen(buf) - 1] = 0; /* replace newline with null */
+
+		if ((pid = fork()) < 0) {
+			err_sys("fork error");
+		} else if (pid == 0) {		/* child */
+			execlp(buf, buf, (char *)0);
+			err_ret("couldn't execute: %s", buf);
+			exit(127);
+		}
+
+		/* parent */
+		if ((pid = waitpid(pid, &status, 0)) < 0)
+			err_sys("waitpid error");
+		printf("%% ");
+	}
+	exit(0);
+}
